8e9175c0d47985d731ac7f58c547862e386067ff
kazimoto5520 <meshackkazimoto@gmail.com>
2026-01-22 17:09:26 +0300

    path redirect

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
